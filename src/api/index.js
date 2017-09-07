18f7f0968b699fec228b47145adf169d9dd47781
john <1585185302@qq.com>
2017-09-07 18:30:46 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 var api = axios.create({
-    baseURL: 'https://api2.laoshi123.com/',
-    // baseURL: 'http://192.168.1.21:4005',
+    baseURL: '/',
     withCredentials: true,
     timeout: 5000,
 });
